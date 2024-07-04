a8ea10d3b725d6be23b2eb241815698f13dfe672
xandaoxl <barrosxloficial@gmail.com>
2024-07-04 11:33:58 -0300

    add

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-// funções anõnimas
+const shoes = [
+    { nome: 'Nike', preco: 200, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore' },
+    { nome: 'Adidas', preco: 120, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore' },
+    { nome: 'Puma', preco: 100, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore'  },
+    { nome: 'Reebok', preco: 80, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore' },
+    { nome: 'Vans', preco: 60, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore'  },
+    { nome: 'Converse', preco: 40, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore'  },
+    { nome: 'Crocs', preco: 1, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore'  }
+]
 
-const sum = function (a,b) {
-    return a + b;
-}
+const shoesList = document.getElementById('shoes-list')
+
+shoes.forEach((shoe) => {
+    const li = document.createElement('li')
+    li.textContent = `${shoe.nome} - $${shoe.preco}`
+    const img = document.createElement('img')
+    img.src = shoe.imagem
+    li.appendChild(img)
+    shoesList.appendChild(li)
+})
+
